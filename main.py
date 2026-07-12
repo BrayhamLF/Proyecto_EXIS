c7893a5511568a773e02d4020f1e2c7bd84d9f12
Brayham Lindarte <143204242+BrayhamLF@users.noreply.github.com>
2026-07-12 13:58:38 -0500

    Main project

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+Punto de entrada de la aplicación.
+"""
+
+from app.application import Application
+
+
+def main():
+
+    app = Application()
+
+    app.mainloop()
+
+
+if __name__ == "__main__":
+
+    main()
+    
